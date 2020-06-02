efd6b89d12c737cb589604f008cd53ff222dd835
Jake Donahue <jdonahue135@gmail.com>
2020-06-02 16:29:24 -0400

    complete home component layout

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -7,11 +7,38 @@ const Home = (props) => {
     <div className="Home">
       <div className="title-container">
         <p className="title">Home</p>
-        <svg className="sort-icon" viewBox="0 0 24 24">
+        <svg className="sort-graphic" viewBox="0 0 24 24">
           <g>
             <path d={svgDimensions.SORT}></path>
           </g>
         </svg>
+      </div>
+      <div className="tweet-compose-container">
+        <div className="pic-container" />
+        <input type="text" placeholder="What's happening?" />
+        <div className="tweet-compose-footer">
+          <div className="tweet-compose-graphic-container">
+            <svg className="tweet-compose-graphic" viewBox="0 0 24 24">
+              <g>
+                <path d={svgDimensions.PHOTO_UPLOAD} />
+                <circle cx="8.868" cy="8.309" r="1.542" />
+              </g>
+            </svg>
+            <svg className="tweet-compose-graphic" viewBox="0 0 24 24">
+              <g>
+                <path d={svgDimensions.GIF1} />
+                <path d={svgDimensions.GIF2} />
+              </g>
+            </svg>
+          </div>
+          <div className="tweet-button tweet-button-sm">
+            <p>Tweet</p>
+          </div>
+        </div>
+      </div>
+      <div className="break" />
+      <div className="main">
+        <div className="tweets-container" />
       </div>
     </div>
   );
