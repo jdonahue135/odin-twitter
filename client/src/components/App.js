a37336fb2008e49191a45bc5c518fd385637bb78
Jake Donahue <jdonahue135@gmail.com>
2020-06-02 16:28:35 -0400

    remove create-react-app content and replace with components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,26 +1,13 @@
 import React from "react";
-import logo from "../images/Twitter_Logo_Blue.png";
 import "../styles/App.css";
 import Sidebar from "./Sidebar";
+import Home from "./Home";
 
 function App() {
   return (
     <div className="App">
       <Sidebar />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home />
     </div>
   );
 }
