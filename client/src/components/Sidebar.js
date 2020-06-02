d59b0189a0034d740c7706ad06a3ea9742517ea8
Jake Donahue <jdonahue135@gmail.com>
2020-06-02 13:32:21 -0400

    finish sidebar layout and add styles

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -6,38 +6,54 @@ import logo from "../images/Twitter_Logo_Blue.png";
 const Sidebar = (props) => {
   //to change icons to blue change the svg style to include "fill: rgba(29,161,242,1.00)", height: 1.75rem
   const makeSidebarIcon = (title) => {
-    if (title === "Home") {
+    if (title === "Home" || title === "More") {
       return (
-        <div>
+        <div className="sidebar-item">
           <svg className="sidebar-icon" viewBox="0 0 24 24">
             <g>
-              <path d={svgDimensions.HOME1}></path>
-              <path d={svgDimensions.HOME2}></path>
+              <path d={svgDimensions[title.toUpperCase() + 1]}></path>
+              <path d={svgDimensions[title.toUpperCase() + 2]}></path>
             </g>
           </svg>
-          <p>{title}</p>
+          <p className="sidebar-label">{title}</p>
         </div>
       );
     }
     return (
-      <div>
+      <div className="sidebar-item">
         <svg className="sidebar-icon" viewBox="0 0 24 24">
           <g>
             <path d={svgDimensions[title.toUpperCase()]}></path>
           </g>
         </svg>
-        <p>{title}</p>
+        <p className="sidebar-label">{title}</p>
       </div>
     );
   };
 
   return (
-    <div className="sidebar">
+    <div className="Sidebar">
       <img className="logo" src={logo} alt="logo" />
       {makeSidebarIcon("Home")}
       {makeSidebarIcon("Notifications")}
       {makeSidebarIcon("Messages")}
       {makeSidebarIcon("Profile")}
+      {makeSidebarIcon("More")}
+      <div className="tweet-button">
+        <p>Tweet</p>
+      </div>
+      <div className="profile">
+        <div className="pic-container"></div>
+        <div className="id-container">
+          <p className="profile-name">Your 14 char N...</p>
+          <p className="profile-handle">@yourHandle</p>
+        </div>
+        <svg className="profile-graphic">
+          <g>
+            <path d={svgDimensions.PROFILE_OPTIONS}></path>
+          </g>
+        </svg>
+      </div>
     </div>
   );
 };
