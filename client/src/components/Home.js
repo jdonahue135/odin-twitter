46d418fa60acbe63735e36f0506c8b8a775654db
Jake Donahue <jdonahue135@gmail.com>
2020-06-04 15:35:42 -0400

    add TweetForm and CharacterCounter components, and keep track of focus on TweetForm to dynamically render content

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,47 +1,85 @@
 import React from "react";
+import TweetForm from "./TweetForm";
+import CharacterCounter from "./CharacterCounter";
 
 import { svgDimensions } from "../constants";
 
-const Home = (props) => {
-  return (
-    <div className="Home">
-      <div className="title-container">
-        <p className="title">Home</p>
-        <svg className="sort-graphic" viewBox="0 0 24 24">
-          <g>
-            <path d={svgDimensions.SORT}></path>
-          </g>
-        </svg>
-      </div>
-      <div className="tweet-compose-container">
-        <div className="pic-container" />
-        <input type="text" placeholder="What's happening?" />
-        <div className="tweet-compose-footer">
-          <div className="tweet-compose-graphic-container">
-            <svg className="tweet-compose-graphic" viewBox="0 0 24 24">
-              <g>
-                <path d={svgDimensions.PHOTO_UPLOAD} />
-                <circle cx="8.868" cy="8.309" r="1.542" />
-              </g>
-            </svg>
-            <svg className="tweet-compose-graphic" viewBox="0 0 24 24">
-              <g>
-                <path d={svgDimensions.GIF1} />
-                <path d={svgDimensions.GIF2} />
-              </g>
-            </svg>
-          </div>
-          <div className="tweet-button tweet-button-sm">
-            <p>Tweet</p>
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      focused: false,
+    };
+  }
+
+  handleFocus() {
+    this.setState({ focused: true });
+  }
+  render() {
+    return (
+      <div className="Home">
+        <div className="title-container">
+          <p className="title">Home</p>
+          <svg className="sort-graphic" viewBox="0 0 24 24">
+            <g>
+              <path d={svgDimensions.SORT}></path>
+            </g>
+          </svg>
+        </div>
+        <div className="tweet-compose-container">
+          <div className="pic-container pic-container-lg" />
+          <TweetForm
+            onChange={this.props.onChange}
+            onFocus={this.handleFocus.bind(this)}
+          />
+          {this.state.focused ? (
+            <div className="visibility-container">
+              <svg className="visibility-graphic" viewBox="0 0 24 24">
+                <g>
+                  <path d={svgDimensions.GLOBE} />
+                </g>
+              </svg>
+              <p className="visibility-message">Everyone can reply</p>
+            </div>
+          ) : (
+            <div />
+          )}
+          {this.state.focused ? <div className="break" /> : <div />}
+
+          <div className="tweet-compose-footer">
+            <div className="tweet-compose-graphic-container">
+              <svg className="tweet-compose-graphic" viewBox="0 0 24 24">
+                <g>
+                  <path d={svgDimensions.PHOTO_UPLOAD} />
+                  <circle cx="8.868" cy="8.309" r="1.542" />
+                </g>
+              </svg>
+              <svg className="tweet-compose-graphic" viewBox="0 0 24 24">
+                <g>
+                  <path d={svgDimensions.GIF1} />
+                  <path d={svgDimensions.GIF2} />
+                </g>
+              </svg>
+            </div>
+            {this.props.charCount > 0 ? (
+              <CharacterCounter charCount={this.props.charCount} />
+            ) : (
+              <div />
+            )}
+
+            <div className="tweet-button tweet-button-sm">
+              <p>Tweet</p>
+            </div>
           </div>
         </div>
+        <div className="break" />
+        <div className="main">
+          <div className="tweets-container" />
+        </div>
       </div>
-      <div className="break" />
-      <div className="main">
-        <div className="tweets-container" />
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Home;
