8bd586a2e40777a6c75c96ea99abc043f3fe1ef4
Jake Donahue <jdonahue135@gmail.com>
2020-06-05 17:04:59 -0400

    add handling of different classes in renderGraphic function

--- client/src/helpers.js
+++ client/src/helpers.js
@@ -1,11 +1,14 @@
 import React from "react";
 
 export const renderGraphic = (graphic) => {
+  const classList = graphic.class
+    ? graphic.type + "-graphic-container " + graphic.class
+    : graphic.type + "-graphic-container";
   const dimensions = graphic.d;
   const circle = graphic.circle ? graphic.circle : null;
   if (Array.isArray(dimensions)) {
     return (
-      <div className={graphic.type + "-graphic-container"}>
+      <div className={classList}>
         <svg className={graphic.type + "-graphic"} viewBox="0 0 24 24">
           <g>
             <path d={dimensions[0]} />
@@ -17,7 +20,7 @@ export const renderGraphic = (graphic) => {
   }
   if (circle) {
     return (
-      <div className={graphic.type + "-graphic-container"}>
+      <div className={classList}>
         <svg className={graphic.type + "-graphic"} viewBox="0 0 24 24">
           <g>
             <path d={dimensions} />
@@ -28,7 +31,7 @@ export const renderGraphic = (graphic) => {
     );
   }
   return (
-    <div className={graphic.type + "-graphic-container"}>
+    <div className={classList}>
       <svg className={graphic.type + "-graphic"} viewBox="0 0 24 24">
         <g>
           <path d={dimensions} />
