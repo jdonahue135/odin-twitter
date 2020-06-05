4fdfb559d16eb6558415785937cd562ad5ff2571
Jake Donahue <jdonahue135@gmail.com>
2020-06-05 17:09:44 -0400

    add route for user information

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var User = require("../models/User");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  User.find({}, function (err, theUsers) {
+    if (err) return next(err);
+    res.json(theUsers);
+  });
+  //res.send('respond with a resource');
 });
 
 module.exports = router;
