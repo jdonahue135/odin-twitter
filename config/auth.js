577d6d0cd4cc8c45d93d1b8f3e7c8452eb812a61
Jake Donahue <jdonahue135@gmail.com>
2020-06-08 17:05:25 -0400

    configure jsonwebtoken

---
+++ config/auth.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+
+// Get Token
+const getToken = (req, res, next) => {
+  const bearerHeader = req.headers["authorization"];
+  // Check if bearer is undefined
+  if (typeof bearerHeader !== "undefined") {
+    // Split at the space
+    const bearer = bearerHeader.split(" ");
+    // Get token from array
+    const bearerToken = bearer[1];
+    // Set the token
+    req.token = bearerToken;
+  }
+  next();
+};
+
+const verifyToken = (req, res, next) => {
+  //verify token
+  jwt.verify(req.token, process.env.JWT_KEY, (err, authData) => {
+    if (err) return next(err);
+  });
+  next();
+};
+
+module.exports = {
+  getToken,
+  verifyToken,
+};
