3bb6836807451c27b462430275495891af3e86ce
Jake Donahue <jdonahue135@gmail.com>
2020-06-08 10:28:26 -0400

    add functionality to dynamically render ProfilePic classes

--- client/src/components/ProfilePic.js
+++ client/src/components/ProfilePic.js
@@ -3,10 +3,11 @@ import React from "react";
 import defaultPhoto from "../images/default_profile_pic.png";
 
 const ProfilePic = (props) => {
+  const classList = "profile-pic profile-pic-" + props.size;
   return (
     <div className="profile-pic-container">
       <img
-        className="profile-pic profile-pic-med"
+        className={classList}
         src={!props.photo ? defaultPhoto : props.photo}
         alt="profile-pic"
       />
