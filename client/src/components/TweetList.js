591694d8b462f5c57bd9ca0e33f865eed2f26213
Jake Donahue <jdonahue135@gmail.com>
2020-06-10 11:57:53 -0400

    add sort function so tweets are in order of most recent

--- client/src/components/TweetList.js
+++ client/src/components/TweetList.js
@@ -3,9 +3,16 @@ import React from "react";
 import Tweet from "./Tweet";
 
 const TweetList = (props) => {
+  const sortedTweets = props.tweets.sort(function (a, b) {
+    a = new Date(a.date);
+    b = new Date(b.date);
+
+    //reverse logic of sort function
+    return a > b ? -1 : a < b ? 1 : 0;
+  });
   return (
     <div className="tweets-container">
-      {props.tweets.map((tweet) => (
+      {sortedTweets.map((tweet) => (
         <Tweet key={tweet._id} tweet={tweet} />
       ))}
     </div>
