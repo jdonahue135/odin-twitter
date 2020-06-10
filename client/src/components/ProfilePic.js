5510ddffbe91599fc176c8d12a2e2f00b97a6b73
Jake Donahue <jdonahue135@gmail.com>
2020-06-10 14:46:37 -0400

    make component stateful and handle hover events on img

--- client/src/components/ProfilePic.js
+++ client/src/components/ProfilePic.js
@@ -2,16 +2,41 @@ import React from "react";
 
 import defaultPhoto from "../images/default_profile_pic.png";
 
-const ProfilePic = (props) => {
-  const classList = "profile-pic profile-pic-" + props.size;
-  return (
-    <div className="profile-pic-container">
-      <img
-        className={classList}
-        src={!props.photo ? defaultPhoto : props.photo}
-        alt="profile-pic"
-      />
-    </div>
-  );
-};
+class ProfilePic extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isHovered: false,
+    };
+  }
+
+  handleHover() {
+    this.setState({ isHovered: true });
+  }
+
+  handleBlur() {
+    this.setState({ isHovered: false });
+  }
+
+  render() {
+    const classList = "profile-pic profile-pic-" + this.props.size;
+    return (
+      <div className="profile-pic-container">
+        {this.state.isHovered ? (
+          <div
+            className="profile-pic-overlay"
+            onMouseLeave={this.handleBlur.bind(this)}
+          />
+        ) : null}
+        <img
+          onMouseEnter={this.handleHover.bind(this)}
+          className={classList}
+          src={!this.props.photo ? defaultPhoto : this.props.photo}
+          alt="profile-pic"
+        />
+      </div>
+    );
+  }
+}
 export default ProfilePic;
