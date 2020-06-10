2ba00e302ea38df3a24f3ecbff8d8efbb98bc49e
Jake Donahue <jdonahue135@gmail.com>
2020-06-10 11:55:28 -0400

    lift state up to App component so all fetch calls for tweets happen in one place

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -7,51 +7,33 @@ import ProfilePic from "./ProfilePic";
 import { graphics } from "../constants";
 import { renderGraphic } from "../helpers";
 
-class Home extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      tweets: null,
-    };
-  }
-
-  componentDidMount() {
-    //fetch tweets
-    fetch("/tweets")
-      .then((res) => res.json())
-      .then((tweets) => this.setState({ tweets }))
-      .catch((err) => console.log(err));
-  }
-
-  render() {
-    return (
-      <div className="Home">
-        <div className="title-container">
-          <p className="title">Home</p>
-          {renderGraphic(graphics.SORT)}
-        </div>
-        <div className="tweet-compose-container">
-          <ProfilePic
-            photo={this.props.user ? this.props.user.profilePicture : null}
-            size="med"
-          />
-          <TweetForm
-            charCount={this.props.charCount}
-            onChange={this.props.onChange}
-            onClick={this.props.onClick}
-          />
-        </div>
-        <div className="main">
-          {this.state.tweets ? (
-            <TweetList tweets={this.state.tweets} />
-          ) : (
-            <div style={{ display: "none" }} />
-          )}
-        </div>
+const Home = (props) => {
+  return (
+    <div className="Home">
+      <div className="title-container">
+        <p className="title">Home</p>
+        {renderGraphic(graphics.SORT)}
       </div>
-    );
-  }
-}
+      <div className="tweet-compose-container">
+        <ProfilePic
+          photo={props.user ? props.user.profilePicture : null}
+          size="med"
+        />
+        <TweetForm
+          charCount={props.charCount}
+          onChange={props.onChange}
+          onClick={props.onClick}
+        />
+      </div>
+      <div className="main">
+        {props.tweets ? (
+          <TweetList tweets={props.tweets} />
+        ) : (
+          <div style={{ display: "none" }} />
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default Home;
