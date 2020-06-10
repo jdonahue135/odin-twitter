7f9ff1aaf3d88296f42dc8ba4ad90ce4c181c452
Jake Donahue <jdonahue135@gmail.com>
2020-06-10 14:43:49 -0400

    make component stateful and add Logout menu styling

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { ClickAwayListener } from "@material-ui/core";
 
 import ProfilePic from "./ProfilePic";
 import Button from "./Button";
@@ -7,9 +8,16 @@ import { graphics } from "../constants";
 import { renderGraphic } from "../helpers";
 import logo from "../images/Twitter_Logo_Blue.png";
 
-const Sidebar = (props) => {
-  //to change icons to blue change the svg style to include "fill: rgba(29,161,242,1.00)", height: 1.75rem
-  const renderSidebarItem = (title) => {
+class Sidebar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isClicked: false,
+    };
+  }
+
+  renderSidebarItem = (title) => {
     return (
       <div className="sidebar-item-container">
         {renderGraphic(graphics[title.toUpperCase()])}
@@ -18,28 +26,52 @@ const Sidebar = (props) => {
     );
   };
 
-  return (
-    <div className="Sidebar">
-      <img className="logo" src={logo} alt="logo" />
-      {renderSidebarItem("Home")}
-      {renderSidebarItem("Notifications")}
-      {renderSidebarItem("Messages")}
-      {renderSidebarItem("Profile")}
-      {renderSidebarItem("More")}
-      <Button size="lg" />
-      <div className="profile">
-        <ProfilePic
-          size="sm"
-          photo={props.user ? props.user.profilePicture : null}
-        />
-        <div className="id-container">
-          <p className="profile-name">Your 14 char N...</p>
-          <p className="profile-handle">@yourHandle</p>
-        </div>
-        {renderGraphic(graphics.PROFILE_OPTIONS)}
+  handleProfileClick() {
+    this.setState({ isClicked: !this.state.isClicked });
+  }
+  handleClickAway() {
+    if (this.state.isClicked === true) {
+      this.setState({ isClicked: false });
+    }
+  }
+
+  render() {
+    const formatName =
+      this.props.username.length > 14
+        ? this.props.username.slice(0, 13) + "..."
+        : this.props.username;
+    return (
+      <div className="Sidebar">
+        <img className="logo" src={logo} alt="logo" />
+        {this.renderSidebarItem("Home")}
+        {this.renderSidebarItem("Notifications")}
+        {this.renderSidebarItem("Messages")}
+        {this.renderSidebarItem("Profile")}
+        {this.renderSidebarItem("More")}
+        <Button size="lg" />
+        {this.state.isClicked ? (
+          <div className="profile-popup">
+            <div onClick={this.props.onClick} className="popup-text-container">
+              {"Log out @" + this.props.handle}
+            </div>
+          </div>
+        ) : null}
+        <ClickAwayListener onClickAway={this.handleClickAway.bind(this)}>
+          <div className="profile" onClick={this.handleProfileClick.bind(this)}>
+            <ProfilePic
+              size="sm"
+              photo={this.props.user ? this.props.user.profilePicture : null}
+            />
+            <div className="id-container">
+              <p className="profile-name">{formatName}</p>
+              <p className="profile-handle">{"@" + this.props.handle}</p>
+            </div>
+            {renderGraphic(graphics.PROFILE_OPTIONS)}
+          </div>
+        </ClickAwayListener>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Sidebar;
