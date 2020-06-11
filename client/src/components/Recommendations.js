a9ddcf67613dc7a7248ffd5888bdb3de85caec22
Jake Donahue <jdonahue135@gmail.com>
2020-06-11 12:31:44 -0400

    implement Recommendations component when Home component is displayed, and dynamically display users to follow

---
+++ client/src/components/Recommendations.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+import User from "./User";
+import SpinningLoader from "./SpinningLoader";
+
+class Recommendations extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      users: null,
+    };
+  }
+
+  componentDidUpdate() {
+    if (!this.state.users) {
+      fetch("/users")
+        .then((res) => res.json())
+        .then((users) => this.setState({ users }))
+        .catch((err) => console.log(err));
+    }
+  }
+  render() {
+    return (
+      <div className="recommendations-container">
+        <div className="user-title-container">
+          <div className="title user-title">Who to follow</div>
+        </div>
+        {this.state.users ? (
+          <div className="users-container">
+            <User user={this.state.users[0]} />
+            <User user={this.state.users[1]} />
+            <User user={this.state.users[2]} />
+            <User user={this.state.users[3]} />
+            <p className="show-more">Show more</p>
+          </div>
+        ) : (
+          <SpinningLoader />
+        )}
+      </div>
+    );
+  }
+}
+
+export default Recommendations;
