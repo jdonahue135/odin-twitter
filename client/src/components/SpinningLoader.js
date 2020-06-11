db105f03819b48fcb245c59d19e2ff38df62d7d8
Jake Donahue <jdonahue135@gmail.com>
2020-06-11 12:32:15 -0400

    add component that displays animation when other components are loading

---
+++ client/src/components/SpinningLoader.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const SpinningLoader = (props) => {
+  //style from https://alligator.io/css/css-spinning-loader-animation/
+  return <div className="spinning-loader" />;
+};
+
+export default SpinningLoader;
