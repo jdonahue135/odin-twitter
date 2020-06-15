b8e5516d3e96535c3bc9b1b8fd731e3dea8f4395
Jake Donahue <jdonahue135@gmail.com>
2020-06-15 17:33:38 -0400

    add new fields for tweet: mentions and isReplyTo

--- models/Tweet.js
+++ models/Tweet.js
@@ -3,13 +3,15 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var TweetSchema = new Schema({
-  user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
   text: { type: String, required: true, max: 280 },
   date: { type: Date, default: Date.now },
-  replies: [ { type: Schema.Types.ObjectId, ref: 'Tweet' } ],
-  retweets: [ { type: Schema.Types.ObjectId, ref: 'Tweet' } ],
-  likes: [ { type: Schema.Types.ObjectId, ref: 'User' } ],
-  photos: [ { type: Buffer } ]
+  replies: [{ type: Schema.Types.ObjectId, ref: "Tweet" }],
+  retweets: [{ type: Schema.Types.ObjectId, ref: "Tweet" }],
+  mentions: [{ type: Schema.Types.ObjectId, ref: "Tweet" }],
+  inReplyTo: { type: Schema.Types.ObjectId, ref: "Tweet" },
+  likes: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  photos: [{ type: Buffer }],
 });
 
 //Export model
