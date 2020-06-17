5e316e623d53aef75aa3be906024958d772efb68
Jake Donahue <jdonahue135@gmail.com>
2020-06-17 10:17:05 -0400

    add model for Notifications

---
+++ models/Notification.js
@@ -0,0 +1,15 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var NotificationSchema = new Schema({
+  readStatus: { type: Boolean, required: true, default: false },
+  date: { type: Date, default: Date.now },
+  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  actionUsers: [{ type: Schema.Types.ObjectId, ref: "User", required: true }],
+  type: { type: String, required: true, max: 15 },
+  tweet: { type: Schema.Types.ObjectId, ref: "Tweet" },
+});
+
+//Export model
+module.exports = mongoose.model("Notification", NotificationSchema);
