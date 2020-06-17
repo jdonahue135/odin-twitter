b0a869071d0ca253ccd92e9796ab224d981bbfd0
Jake Donahue <jdonahue135@gmail.com>
2020-06-17 16:26:09 -0400

    make component stateful and add conditional formatting

--- client/src/components/User.js
+++ client/src/components/User.js
@@ -3,22 +3,50 @@ import React from "react";
 import ProfilePic from "./ProfilePic";
 import Button from "./Button";
 
-const User = (props) => {
-  const name =
-    props.user.name.length > 14
-      ? props.user.name.slice(0, 13)
-      : props.user.name;
-  return (
-    <div className="user-container">
-      <ProfilePic size="med" />
-      <div className="profile-information-container">
-        <div className="profile-name recommendations-profile-name">{name}</div>
-        <div className="profile-handle">{"@" + props.user.username}</div>
-        {props.showBio ? <div>{props.user.bio}</div> : null}
+class User extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      reRender: false,
+    };
+  }
+
+  render() {
+    const name =
+      this.props.user.name.length > 14
+        ? this.props.user.name.slice(0, 13)
+        : this.props.user.name;
+    const buttonTextContent = this.props.currentUser.following.includes(
+      this.props.user._id
+    )
+      ? "Following"
+      : "Follow";
+    const buttonClass = this.props.currentUser.following.includes(
+      this.props.user._id
+    )
+      ? "unfollow-btn"
+      : "follow-btn";
+    return (
+      <div className="user-container">
+        <ProfilePic size="med" />
+        <div className="profile-information-container">
+          <div className="profile-name recommendations-profile-name">
+            {name}
+          </div>
+          <div className="profile-handle">{"@" + this.props.user.username}</div>
+          {this.props.showBio ? <div>{this.props.user.bio}</div> : null}
+        </div>
+        <Button
+          id={this.props.user._id}
+          textContent={buttonTextContent}
+          class={buttonClass}
+          size="sm"
+          onClick={this.props.onClick}
+        />
       </div>
-      <Button textContent="Follow" class="follow-btn" size="sm" />
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default User;
