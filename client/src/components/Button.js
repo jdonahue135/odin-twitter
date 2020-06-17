6fd948de860d91956a13c95ba0d42b0ecdb1bcc8
Jake Donahue <jdonahue135@gmail.com>
2020-06-17 16:22:54 -0400

    add more conditional formatting to handle follow buttons

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -1,15 +1,37 @@
 import React from "react";
 
-const Button = (props) => {
-  const textContent = props.textContent ? props.textContent : "Tweet";
-  return (
-    <div
-      onClick={props.onClick}
-      className={"btn btn-" + props.size + " " + props.class}
-    >
-      {textContent}
-    </div>
-  );
-};
+class Button extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isHovered: false,
+    };
+  }
+
+  toggleHover() {
+    this.setState({ isHovered: !this.state.isHovered });
+  }
+  render() {
+    let textContent = this.props.textContent ? this.props.textContent : "Tweet";
+    let style;
+    if (textContent === "Following" && this.state.isHovered) {
+      textContent = "Unfollow";
+      style = { backgroundColor: "rgb(202, 32, 85)", color: "white" };
+    }
+    return (
+      <div
+        onClick={this.props.onClick}
+        className={"btn btn-" + this.props.size + " " + this.props.class}
+        id={this.props.id}
+        onMouseEnter={this.toggleHover.bind(this)}
+        onMouseLeave={this.toggleHover.bind(this)}
+        style={style}
+      >
+        {textContent}
+      </div>
+    );
+  }
+}
 
 export default Button;
