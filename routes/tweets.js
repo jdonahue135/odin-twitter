df71b878b5ce144e4051a10cba0352dd0aecb749
Jake Donahue <jdonahue135@gmail.com>
2020-06-18 17:39:58 -0400

    add controller function for tweet GET requests

--- routes/tweets.js
+++ routes/tweets.js
@@ -4,16 +4,8 @@ var Tweet = require("../models/Tweet");
 
 var tweets_controller = require("../controllers/tweetsController");
 
-/* GET all tweets. */
-router.get("/", function (req, res, next) {
-  Tweet.find({})
-    .populate("user")
-    .exec(function (err, theTweets) {
-      if (err) return next(err);
-      res.json(theTweets);
-    });
-  //res.send('respond with a resource');
-});
+/* GET all user following tweets. */
+router.get("/:userid", tweets_controller.tweets_get);
 
 /* POST new tweet */
 router.post("/", tweets_controller.tweet_post);
