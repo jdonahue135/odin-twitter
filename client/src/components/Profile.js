572dae47ce305ca5435199893779f7df50322876
Jake Donahue <jdonahue135@gmail.com>
2020-06-22 11:01:16 -0400

    send user tweets as prop and remove API calls from component

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -16,16 +16,7 @@ class Profile extends React.Component {
 
     this.state = {
       tweetsSelected: true,
-      tweets: null,
     };
-  }
-
-  componentDidMount() {
-    //get user info and user tweets
-    fetch("/users/" + this.props.match.params.username + "/tweets")
-      .then((res) => res.json())
-      .then((tweets) => this.setState({ tweets }))
-      .catch((err) => console.log(err));
   }
 
   handleClick(e) {
@@ -37,7 +28,7 @@ class Profile extends React.Component {
   }
 
   render() {
-    if (!this.state.tweets) {
+    if (!this.props.tweets) {
       return (
         <div className="component">
           <div className="spinning-loader" />
@@ -45,24 +36,24 @@ class Profile extends React.Component {
       );
     }
     const tweetCount =
-      this.state.tweets.length === 1
+      this.props.tweets.length === 1
         ? "1 Tweet"
-        : this.state.tweets.length + " tweets";
+        : this.props.tweets.length + " tweets";
     return (
       <div className="component">
         <div className="title-container profile-title-container">
           {renderGraphic(graphics.BACK)}
           <div className="profile-info-container">
             <p className="title profile-title">
-              {this.state.tweets[0].user.name}
+              {this.props.tweets[0].user.name}
             </p>
             <p className="sub-title">{tweetCount}</p>
           </div>
         </div>
         <div className="main profile-main">
           <div className="header-image-container">
-            {this.state.tweets[0].user.headerImage ? (
-              <img src={this.state.tweets[0].user.headerImage} alt="header" />
+            {this.props.tweets[0].user.headerImage ? (
+              <img src={this.props.tweets[0].user.headerImage} alt="header" />
             ) : (
               <div className="default-header-image" />
             )}
@@ -71,10 +62,10 @@ class Profile extends React.Component {
           <Button textContent="Set up profile" size="med" class="follow-btn" />
           <div className="profile-main-info-container">
             <p className="title profile-main-title">
-              {this.state.tweets[0].user.name}
+              {this.props.tweets[0].user.name}
             </p>
             <p className="profile-handle">
-              {"@" + this.state.tweets[0].user.username}
+              {"@" + this.props.tweets[0].user.username}
             </p>
             <div className="calendar-container">
               <img className="calendar-graphic" src={calendar} alt="calendar" />
@@ -83,13 +74,13 @@ class Profile extends React.Component {
             <div className="follow-info-container">
               <div className="follow-count-item">
                 <p className="follow-count">
-                  {this.state.tweets[0].user.following.length}
+                  {this.props.tweets[0].user.following.length}
                 </p>
                 <p className="follow-count-label">&nbsp;Following</p>
               </div>
               <div className="follow-count-item">
                 <p className="follow-count">
-                  {this.state.tweets[0].user.followers.length}
+                  {this.props.tweets[0].user.followers.length}
                 </p>
                 <p className="follow-count-label">&nbsp;Followers</p>
               </div>
@@ -99,13 +90,13 @@ class Profile extends React.Component {
             a="Tweets"
             b="Tweets & replies"
             class="profile-options"
-            selected={this.state.tweetsSelected}
+            selected={this.props.tweetsSelected}
             onClick={this.handleClick.bind(this)}
           />
-          {this.state.tweets.length > 0 ? (
+          {this.props.tweets.length > 0 ? (
             <TweetList
               user={this.props.user}
-              tweets={this.state.tweets}
+              tweets={this.props.tweets}
               class="profile"
               deleteTweet={this.props.onTweetDelete}
               onFollowChange={this.props.onClick}
