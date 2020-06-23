ba9b291c7daf9a95e412e5a649d67da2c1c6cbf3
Jake Donahue <jdonahue135@gmail.com>
2020-06-23 15:00:45 -0400

    remove unnecessary fields and add better ones for retweet handling

--- models/Tweet.js
+++ models/Tweet.js
@@ -4,11 +4,11 @@ var Schema = mongoose.Schema;
 
 var TweetSchema = new Schema({
   user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  text: { type: String, required: true, max: 280 },
+  text: { type: String, max: 280 },
   date: { type: Date, default: Date.now },
+  retweets: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  retweetOf: { type: Schema.Types.ObjectId, ref: "Tweet" },
   replies: [{ type: Schema.Types.ObjectId, ref: "Tweet" }],
-  retweets: [{ type: Schema.Types.ObjectId, ref: "Tweet" }],
-  mentions: [{ type: Schema.Types.ObjectId, ref: "Tweet" }],
   inReplyTo: { type: Schema.Types.ObjectId, ref: "Tweet" },
   likes: [{ type: Schema.Types.ObjectId, ref: "User" }],
   photos: [{ type: Buffer }],
