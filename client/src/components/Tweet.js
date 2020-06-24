c481dc7fec3c016947bc26507cabe0cd28a23f64
Jake Donahue <jdonahue135@gmail.com>
2020-06-24 12:40:20 -0400

    seperate tweet footer from Tweet component for better use accross other components

--- client/src/components/Tweet.js
+++ client/src/components/Tweet.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import { ClickAwayListener } from "@material-ui/core";
 
 import ProfilePic from "./ProfilePic";
-import { renderGraphic, formatDate } from "../helpers";
+import { renderGraphic, formatDate, formatTweetText } from "../helpers";
 import { graphics } from "../constants";
 
 class Tweet extends React.Component {
@@ -13,104 +13,67 @@ class Tweet extends React.Component {
 
     this.state = {
       showPopup: false,
-      likeStatus: false,
-      likeCount: null,
-      retweetStatus: false,
-      retweetCount: null,
     };
-  }
-
-  componentDidMount() {
-    const likeStatus = this.props.tweet.likes.includes(
-      this.props.currentUser._id
-    )
-      ? true
-      : false;
-    const retweetStatus = this.props.tweet.retweets.includes(
-      this.props.currentUser._id
-    )
-      ? true
-      : false;
-    this.setState({
-      likeStatus: likeStatus,
-      retweetStatus: retweetStatus,
-      likeCount: this.props.tweet.likes.length,
-      retweetCount: this.props.tweet.retweets.length,
-    });
   }
 
   togglePopup() {
     this.setState({ showPopup: !this.state.showPopup });
   }
 
-  handleLikeChange() {
-    this.props.onLike(this.props.tweet._id);
-    const likeCount = this.state.likeStatus
-      ? this.state.likeCount - 1
-      : this.state.likeCount + 1;
-    this.setState({
-      likeStatus: !this.state.likeStatus,
-      likeCount: likeCount,
-    });
-  }
+  formatText(string) {
+    //deconstruct string into words
+    const words = string.split(" ");
 
-  handleRetweetChange() {
-    const retweetCount = this.state.retweetStatus
-      ? this.state.retweetCount - 1
-      : this.state.retweetCount + 1;
-    this.setState({
-      retweetStatus: !this.state.retweetStatus,
-      retweetCount: retweetCount,
-    });
-    this.props.onRetweet(this.props.tweet._id);
-  }
+    //apply span stylings
+    let formatArray = [];
+    for (let i = 0; i < words.length; i++) {
+      if (words[i][0] === "#") {
+        formatArray.push(
+          <span key={words[i] + " " + i} className="hashtag">
+            {words[i]}
+          </span>,
+          " "
+        );
+      } else formatArray.push(words[i], " ");
+    }
 
-  renderTweetGraphic(name) {
-    const selected = this.state[name + "Status"] ? true : false;
-    const functionString =
-      "handle" + name[0].toUpperCase() + name.slice(1) + "Change";
-    return (
-      <div className="tweet-graphic-item">
-        {renderGraphic(
-          graphics[name.toUpperCase()],
-          selected,
-          this[functionString].bind(this)
-        )}
+    //remove trailing space
+    formatArray[formatArray.length - 1].trim();
 
-        {this.state[name + "Count"] > 0 ? (
-          <div className={"tweet-footer-count " + name + "-count"}>
-            {this.state[name + "Count"]}
-          </div>
-        ) : null}
-      </div>
-    );
+    return formatArray;
   }
 
   render() {
     const tweet = this.props.tweet.retweetOf
       ? this.props.tweet.retweetOf
       : this.props.tweet;
+
+    const text =
+      tweet.text.indexOf("#") !== -1 ? formatTweetText(tweet.text) : tweet.text;
+
     let id;
     let classList = "popup tweet-popup";
     let tweetOptionsText;
-
-    if (this.props.currentUser._id === tweet.user._id) {
-      tweetOptionsText = "Delete";
-      id = tweet._id;
-      classList = classList + " delete-tweet-popup";
-    } else {
-      id = tweet.user._id;
-      classList = classList + " popup tweet-popup";
-      tweetOptionsText = this.props.currentUser.following.includes(
-        tweet.user._id
-      )
-        ? "Unfollow @" + tweet.user.username
-        : "Follow @" + tweet.user.username;
+    let graphic;
+    if (this.props.currentUser) {
+      if (this.props.currentUser._id === tweet.user._id) {
+        tweetOptionsText = "Delete";
+        id = tweet._id;
+        classList = classList + " delete-tweet-popup";
+      } else {
+        id = tweet.user._id;
+        classList = classList + " popup tweet-popup";
+        tweetOptionsText = this.props.currentUser.following.includes(
+          tweet.user._id
+        )
+          ? "Unfollow @" + tweet.user.username
+          : "Follow @" + tweet.user.username;
+      }
+      graphic =
+        this.props.currentUser._id === tweet.user._id
+          ? "DELETE"
+          : tweetOptionsText.replace(/ .*/, "").toUpperCase();
     }
-    const graphic =
-      this.props.currentUser._id === tweet.user._id
-        ? "DELETE"
-        : tweetOptionsText.replace(/ .*/, "").toUpperCase();
 
     return (
       <div>
@@ -131,41 +94,37 @@ class Tweet extends React.Component {
             <div className="tweet-overlay" />
           </div>
         ) : null}
-        <div className="tweet-container">
-          {this.props.tweet.retweetOf ? (
-            <Link to={"/" + this.props.tweet.user.username}>
-              <div className="retweet-label">
-                {renderGraphic(graphics.RETWEET_SMALL)}
-                <div>
-                  {this.props.currentUser.name === this.props.tweet.user.name
-                    ? "You Retweeted"
-                    : this.props.tweet.user.name + " Retweeted"}
-                </div>
+        {this.props.tweet.retweetOf ? (
+          <Link to={"/" + this.props.tweet.user.username}>
+            <div className="retweet-label">
+              {renderGraphic(graphics.RETWEET_SMALL)}
+              <div>
+                {this.props.currentUser.name === this.props.tweet.user.name
+                  ? "You Retweeted"
+                  : this.props.tweet.user.name + " Retweeted"}
               </div>
-            </Link>
-          ) : null}
-          <Link
-            to={"/" + tweet.user.username}
-            onClick={this.props.handlePathChange}
-          >
-            <ProfilePic
-              photo={
-                tweet.user.profilePicture ? tweet.user.profilePicture : null
-              }
-              size="med"
-            />
+            </div>
           </Link>
-          <div className="tweet-main">
-            <div className="tweet-header">
-              <Link
-                to={"/" + tweet.user.username}
-                onClick={this.props.onPathChange}
-              >
-                <div className="tweet-name">{tweet.user.name}</div>
-                <div className="tweet-username">
-                  {"@" + tweet.user.username}
-                </div>
-              </Link>
+        ) : null}
+        <Link
+          to={"/" + tweet.user.username}
+          onClick={this.props.handlePathChange}
+        >
+          <ProfilePic
+            photo={tweet.user.profilePicture ? tweet.user.profilePicture : null}
+            size="med"
+          />
+        </Link>
+        <div className="tweet-main">
+          <div className="tweet-header">
+            <Link
+              to={"/" + tweet.user.username}
+              onClick={this.props.onPathChange}
+            >
+              <div className="tweet-name">{tweet.user.name}</div>
+              <div className="tweet-username">{"@" + tweet.user.username}</div>
+            </Link>
+            <Link to={"/" + tweet.user.username + "/" + tweet._id}>
               <div className="divider">.</div>
               <div className="tweet-date">{formatDate(tweet.date)}</div>
               <div
@@ -174,19 +133,11 @@ class Tweet extends React.Component {
               >
                 {renderGraphic(graphics.TWEET_OPTIONS)}
               </div>
-            </div>
-            <div className="tweet-text">{tweet.text}</div>
-            <div className="tweet-footer">
-              <div className="tweet-graphic-item">
-                {renderGraphic(graphics.REPLY)}
-              </div>
-              {this.renderTweetGraphic("retweet")}
-              {this.renderTweetGraphic("like")}
-              <div className="tweet-graphic-item">
-                {renderGraphic(graphics.SHARE)}
-              </div>
-            </div>
+            </Link>
           </div>
+          <Link to={"/" + tweet.user.username + "/" + tweet._id}>
+            <div className="tweet-text">{text}</div>
+          </Link>
         </div>
       </div>
     );
---
+++ client/src/components/TweetFooter.js
@@ -0,0 +1,101 @@
+import React from "react";
+import { renderGraphic } from "../helpers";
+import { graphics } from "../constants";
+
+class TweetFooter extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      //tracking status and counts so page updates immediately onClick
+      likeStatus: false,
+      likeCount: null,
+      retweetStatus: false,
+      retweetCount: null,
+      //don't need to track replyStatus
+      replyCount: null,
+    };
+  }
+
+  componentDidMount() {
+    const likeStatus = this.props.tweet.likes.includes(
+      this.props.currentUser._id
+    )
+      ? true
+      : false;
+    const retweetStatus = this.props.tweet.retweets.includes(
+      this.props.currentUser._id
+    )
+      ? true
+      : false;
+
+    this.setState({
+      likeStatus: likeStatus,
+      retweetStatus: retweetStatus,
+      likeCount: this.props.tweet.likes.length,
+      retweetCount: this.props.tweet.retweets.length,
+      replyCount: this.props.tweet.replies.length,
+    });
+  }
+  handleLikeClick() {
+    this.props.onLike(this.props.tweet._id);
+    const likeCount = this.state.likeStatus
+      ? this.state.likeCount - 1
+      : this.state.likeCount + 1;
+    this.setState({
+      likeStatus: !this.state.likeStatus,
+      likeCount: likeCount,
+    });
+  }
+
+  handleRetweetClick() {
+    const retweetCount = this.state.retweetStatus
+      ? this.state.retweetCount - 1
+      : this.state.retweetCount + 1;
+    this.setState({
+      retweetStatus: !this.state.retweetStatus,
+      retweetCount: retweetCount,
+    });
+    this.props.onRetweet(this.props.tweet._id);
+  }
+
+  handleReplyClick() {
+    this.props.onReply(this.props.tweet);
+  }
+
+  renderTweetGraphic(name) {
+    const selected = this.state[name + "Status"] ? true : false;
+    const functionString =
+      "handle" + name[0].toUpperCase() + name.slice(1) + "Click";
+    return (
+      <div className="tweet-graphic-item">
+        {renderGraphic(
+          graphics[name.toUpperCase()],
+          selected,
+          this[functionString].bind(this)
+        )}
+
+        {this.state[name + "Count"] > 0 ? (
+          <div className={"tweet-footer-count " + name + "-count"}>
+            {this.state[name + "Count"]}
+          </div>
+        ) : null}
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div className="tweet-footer">
+        {this.renderTweetGraphic("reply")}
+        {this.renderTweetGraphic("retweet")}
+        {this.renderTweetGraphic("like")}
+        <div className="tweet-graphic-item">
+          {renderGraphic(graphics.SHARE)}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default TweetFooter;
