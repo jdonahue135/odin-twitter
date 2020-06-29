2e2efb5db10c8653703d2eceaed046bcfb0b8498
Jake Donahue <jdonahue135@gmail.com>
2020-06-29 16:18:16 -0400

    add photo uploading middleware to tweet post route

--- routes/tweets.js
+++ routes/tweets.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 const auth = require("../config/auth");
+const file_upload = require("../config/file_upload");
 
 var tweets_controller = require("../controllers/tweetsController");
 
@@ -11,7 +12,12 @@ router.get("/:userid", tweets_controller.tweets_get);
 router.get("/status/:tweetid", tweets_controller.tweet_get);
 
 /* POST new tweet */
-router.post("/", auth.verifyToken, tweets_controller.tweet_post);
+router.post(
+  "/",
+  auth.verifyToken,
+  file_upload.upload.single("photo"),
+  tweets_controller.tweet_post
+);
 
 /* handle tweet DELETE on POST */
 router.post(
