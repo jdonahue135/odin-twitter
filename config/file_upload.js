64ffc8b94d26e4d3137fcee6edfa4df1c818ecc6
Jake Donahue <jdonahue135@gmail.com>
2020-06-29 12:27:55 -0400

    implement multer to save uploaded images

---
+++ config/file_upload.js
@@ -0,0 +1,39 @@
+//adapted from tutorial: https://www.positronx.io/react-file-upload-tutorial-with-node-express-and-multer/
+
+var multer = require("multer");
+const { uuid } = require("uuidv4");
+
+const DIR = "./public/images/";
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    //works
+    cb(null, DIR);
+  },
+  filename: (req, file, cb) => {
+    //works
+    const fileName = file.originalname.toLowerCase().split(" ").join("-");
+    cbValue = uuid() + "-" + fileName;
+    cb(null, cbValue);
+  },
+});
+
+var upload = multer({
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+    if (
+      file.mimetype == "image/png" ||
+      file.mimetype == "image/jpg" ||
+      file.mimetype == "image/jpeg"
+    ) {
+      //works
+      console.log(file);
+      cb(null, true);
+    } else {
+      cb(null, false);
+      return cb(new Error("Only .png, .jpg and .jpeg format allowed!"));
+    }
+  },
+});
+
+module.exports = { upload };
