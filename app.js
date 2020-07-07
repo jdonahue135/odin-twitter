37c2f4dd85f3708e13fbe7044252ed391a22ce7a
Jake Donahue <jdonahue135@gmail.com>
2020-07-07 14:35:22 -0400

    add support for react-router in heroku build

--- app.js
+++ app.js
@@ -43,6 +43,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(auth.getToken);
 app.use(express.static(path.join(__dirname, "client/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/build/index.html"));
+});
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
